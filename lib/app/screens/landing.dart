6a3880987bbe86fdd24e039eca5205ab5ff7f1d9
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 01:10:56 +0430

    Add listen to value

--- lib/app/screens/landing.dart
+++ lib/app/screens/landing.dart
@@ -3,6 +3,10 @@ import 'package:flutter/material.dart';
 import 'package:flutter_http_course/app/screens/auth/auth_screen.dart';
 import 'package:flutter_http_course/app/screens/home/home_screen.dart';
 
+import 'package:flutter_http_course/app/services/state.dart';
+
+import 'package:provider/provider.dart';
+
 class Landing extends StatefulWidget {
   const Landing({Key? key}) : super(key: key);
 
@@ -11,10 +15,10 @@ class Landing extends StatefulWidget {
 }
 
 class _LandingState extends State<Landing> {
-  bool auth = true;
-
   @override
   Widget build(BuildContext context) {
+    var auth = Provider.of<AppState>(context, listen: false).getAuth;
+    
     return auth ? const HomeScreen() : const AuthScreen();
   }
 }
