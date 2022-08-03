b86bc6d6f93ebe5fb1dd7956a842da2a4f8d0c00
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 01:11:09 +0430

    Add login

--- lib/app/screens/auth/auth_screen.dart
+++ lib/app/screens/auth/auth_screen.dart
@@ -1,7 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_http_course/app/services/api.dart';
 
-class AuthScreen extends StatelessWidget {
+import 'package:flutter_http_course/app/services/state.dart';
+import 'package:provider/provider.dart';
+
+class AuthScreen extends StatefulWidget {
   const AuthScreen({Key? key}) : super(key: key);
+
+  @override
+  State<AuthScreen> createState() => _AuthScreenState();
+}
+
+class _AuthScreenState extends State<AuthScreen> {
+  final DioClient _client = DioClient();
+
+  void _login(BuildContext context) {
+    Provider.of<AppState>(context, listen: false).loginAuth();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -10,8 +25,11 @@ class AuthScreen extends StatelessWidget {
         elevation: 0,
         title: const Text("Authenticate")
       ),
-      body: const Center(
-        child: Text("Auth Screen"),
+      body: Center(
+        child:ElevatedButton(
+          onPressed: () => _login(context),
+          child: const Text("Test API")
+        ),
       ),
     );
   }
