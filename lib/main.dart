43c8befd58ca9611a153713bb71ea790f889d226
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 01:10:33 +0430

    Added provider in entire app

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_http_course/app/screens/landing.dart';
+import 'package:flutter_http_course/app/services/state.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,13 +13,20 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Learning APIs in Flutter',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.indigo,
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider.value(
+          value: AppState(),
+        )
+      ],
+      child: MaterialApp(
+        title: 'Learning APIs in Flutter',
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: Colors.indigo,
+        ),
+        home: const Landing(),
       ),
-      home: const Landing(),
     );
   }
 }
