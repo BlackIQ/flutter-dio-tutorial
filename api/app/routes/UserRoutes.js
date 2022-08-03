037220f171a3fbb086faf0fb29677097d7adab7b
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-03 21:01:29 +0430

    Add test route

--- api/app/routes/UserRoutes.js
+++ api/app/routes/UserRoutes.js
@@ -5,5 +5,6 @@ const controllers = require('../controllers/UserControllers');
 const router = express.Router();
 
 router.post('/login', controllers.login);
+router.get('/test', controllers.test);
 
 module.exports = router;
