cfdd38651b5e8b948caef7f5fb02c137d4c6fe6b
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 16:37:07 +0430

    Sample settings

--- lib/app/screens/home/tabs/settings_tab.dart
+++ lib/app/screens/home/tabs/settings_tab.dart
@@ -13,11 +13,36 @@ class SettingsTab extends StatefulWidget {
 class _SettingsTabState extends State<SettingsTab> {
   @override
   Widget build(BuildContext context) {
+    var _user = Provider.of<AppState>(context).getUser;
+
+    print(_user);
+    
     return Padding(
       padding: const EdgeInsets.all(20),
       child: Column(
-        children: const [
-          Text("Settings Screen"),
+        children: [
+          const CircleAvatar(
+            child: Icon(
+              Icons.person,
+              size: 30,
+            ),
+            radius: 30,
+          ),
+          const SizedBox(height: 20),
+          Text(
+            "Name: ${_user['name']}",
+            style: const TextStyle(
+              fontSize: 20,
+            ),
+          ),
+          Text(
+            "Account ID: ${_user['_id']}",
+            style: const TextStyle(
+              fontSize: 15,
+            ),
+          ),
+          SizedBox(height: 20),
+          Divider(color: Colors.indigo),
         ],
       ),
     );
