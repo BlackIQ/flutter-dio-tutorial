0535b6eb06711d30d63d305f461b4995a60f5d6f
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 16:37:16 +0430

    Add get all users

--- lib/app/screens/home/tabs/search_tab.dart
+++ lib/app/screens/home/tabs/search_tab.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_http_course/app/services/api.dart';
 
 import 'package:flutter_http_course/app/services/state.dart';
 import 'package:provider/provider.dart';
@@ -11,15 +12,68 @@ class SearchTab extends StatefulWidget {
 }
 
 class _SearchTabState extends State<SearchTab> {
+  final DioClient _client = DioClient();
+
+  List users = [];
+
+  void getUsers() {
+    var res = _client.getUsers();
+
+    res.then((result) {
+      if (result.statusCode == 200) {
+        var data = result.data;
+        setState(() => users = data);
+      }
+    });
+  }
+
+  final TextEditingController _username = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(20),
-      child: Column(
-        children: const [
-          Text("Search Screen"),
-        ],
-      ),
+    getUsers();
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(15),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text("Search the username"),
+              const SizedBox(height: 20),
+              TextField(
+                controller: _username,
+                obscureText: false,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                  label: Text("Username"),
+                  hintText: "Search username",
+                ),
+              ),
+            ],
+          ),
+        ),
+        const Divider(),
+        users.isNotEmpty
+        ?
+        Expanded(
+            child: ListView.builder(
+              itemCount: users.length,
+              itemBuilder: (BuildContext context, int index) {
+                return ListTile(
+                  title: Text(users[index]["name"].toString()),
+                  subtitle: Text(users[index]["username"]),
+                  onTap: () {},
+                );
+              },
+            ),
+          )
+        :
+        const Center(child: CircularProgressIndicator()),
+      ],
     );
   }
 }
