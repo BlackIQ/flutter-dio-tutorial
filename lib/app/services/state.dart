78c59c39d15677a0946bfcca1c40417c41f4e07c
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 13:45:52 +0430

    Add get id

--- lib/app/services/state.dart
+++ lib/app/services/state.dart
@@ -2,16 +2,22 @@ import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
   var _auth = false;
+  var _id;
 
   bool get getAuth {
     return _auth;
   }
+
+  String get getId {
+    return _id;
+  }
   
-  void loginAuth() {
+  void loginAuth(String id) {
     _auth = true;
+    _id = id;
     notifyListeners();
   }
-  void logooutAuth() {
+  void logoutAuth() {
     _auth = false;
     notifyListeners();
   }
