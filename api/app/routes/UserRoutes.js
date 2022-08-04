bad9888c17580dccf0e6e3dc8cd2155884ae1dbe
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 16:36:26 +0430

    Add new routes for get-user, update-user, users

--- api/app/routes/UserRoutes.js
+++ api/app/routes/UserRoutes.js
@@ -6,5 +6,8 @@ const router = express.Router();
 
 router.post('/login', controllers.login);
 router.post('/register', controllers.register);
+router.put('/change', controllers.update);
+router.get('/get/users', controllers.users);
+router.get('/get/user/:user_id', controllers.user);
 
 module.exports = router;
