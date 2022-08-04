e3430abe2d973e54e8d12e27e8176192f4614df3
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 15:40:34 +0430

    Update stuff in state

--- lib/app/services/state.dart
+++ lib/app/services/state.dart
@@ -1,24 +1,30 @@
 import 'package:flutter/material.dart';
 
 class AppState extends ChangeNotifier {
-  var _auth = false;
-  var _id;
+  bool _auth = true;
+  Map _user = { "_id": "50", "name": "amir" };
+  String _id = "50";
 
   bool get getAuth {
     return _auth;
   }
-
   String get getId {
     return _id;
   }
+  Map get getUser {
+    return _user;
+  }
   
-  void loginAuth(String id) {
+  void loginAuth(Map user) {
     _auth = true;
-    _id = id;
+    _id = user["_id"];
+    _user = user;
     notifyListeners();
   }
   void logoutAuth() {
     _auth = false;
+    _id = "";
+    _user = {};
     notifyListeners();
   }
 }
