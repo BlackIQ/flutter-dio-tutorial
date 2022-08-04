836976d5e015456a17f1d6d0dc188aa952adfaf7
AmirHossein Mohammadi <amirhosseinmohammadi1380@yahoo.com>
2022-08-04 13:45:21 +0430

    Return data

--- lib/app/services/api.dart
+++ lib/app/services/api.dart
@@ -1,37 +1,48 @@
-import 'dart:convert';
-import 'dart:developer';
 import 'package:dio/dio.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_http_course/app/services/state.dart';
+import 'package:provider/provider.dart';
 
 class DioClient {
   final Dio _dio = Dio();
 
   final _baseUrl = 'http://192.168.1.5:8000/api';
 
-  Future<int> login(Map data) async {
+  Future<Response> login(String username, String password) async {
+    Map data = {
+      "username": username,
+      "password": password,
+    };
+
     Response response;
 
     try {
       response = await _dio.post('$_baseUrl/user/login', data: data);
 
-      return 200;
+      return response;
     } on DioError catch (e) {
       response = e.response!;
 
-      return 401;
+      return response;
     }
   }
 
-  Future<int> register(Map data) async {
+  void register(String username, String password) async {
+    Map data = {
+      "username": username,
+      "password": password,
+    };
+
     Response response;
 
     try {
       response = await _dio.post('$_baseUrl/user/register', data: data);
 
-      return 200;
+      print(response);
     } on DioError catch (e) {
       response = e.response!;
 
-      return 401;
+      print(response);
     }
   }
 }
